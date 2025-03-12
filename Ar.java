779fb6e8eaa9d43aab832522d82c300be602bd60
Roberto <161033388+Hubierto@users.noreply.github.com>
2025-03-12 11:12:02 -0300

    Add files via upload

---
+++ Ar.java
@@ -0,0 +1,61 @@
+public class Ar {
+
+    private String modelo; 
+    private String codigo;
+    private String marca;
+    private String btu;
+    private String defeito;
+
+    public Ar(String modelo, String codigo, String marca, String btu,  String defeito) {
+        this.modelo = modelo;
+        this.codigo = codigo;
+        this.marca = marca;
+        this.btu = btu;
+        this.defeito = defeito;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getBtu() {
+        return btu;
+    }
+
+    public void setBtu(String btu) {
+        this.btu = btu;
+    }
+
+    public String getDefeito() {
+        return defeito;
+    }
+
+    public void setDefeito(String defeito) {
+        this.defeito = defeito;
+    }
+
+    public void exibInfo(){
+        System.out.println("Modelo: "+ getModelo()+ "\n Código" + getCodigo() + "\nMarca: "+getMarca() + "\n BTU: "+ getBtu() + "\n Defeito: "+getDefeito());
+    }
+    
+}
