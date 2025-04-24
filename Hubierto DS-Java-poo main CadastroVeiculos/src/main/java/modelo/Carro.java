656246d4588a35f1c77f925b5fec5d54c44a7066
Roberto <161033388+Hubierto@users.noreply.github.com>
2025-04-24 09:33:42 -0300

    Add files via upload

---
+++ Hubierto DS-Java-poo main CadastroVeiculos/src/main/java/modelo/Carro.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+public class Carro extends Veiculo {
+    private int numeroPortas;
+
+    public Carro(String marca, String modelo, int numeroPortas) {
+        super(marca, modelo);
+        this.numeroPortas = numeroPortas;
+    }
+
+    public int getNumeroPortas() {
+        return numeroPortas;
+    }
+
+    @Override
+    public String getDescricao() {
+        return "Carro - Marca: " + marca + ", Modelo: " + modelo + ", Portas: " + numeroPortas;
+    }
+}
+
