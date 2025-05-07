e3ca8b677e71b256943033938ca7b61e1858820e
Roberto <161033388+Hubierto@users.noreply.github.com>
2025-05-07 10:03:13 -0300

    Add files via upload

---
+++ Hubierto DS-Java-poo main Hubierto DS-Java-poo main CadastroVeiculos/src/main/java/com/mycompany/cadastroveiculos/CadastroVeiculos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cadastroveiculos;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class CadastroVeiculos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
